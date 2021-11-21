fa6f1db7c06e25bd28dcd9a489be3d809e4606f6
unknown <chadiegilaugis@gmail.com>
2021-11-21 20:37:27 +0800

    semi

---
+++ app/Http/Controllers/VisitorController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Visitor;
+use Illuminate\Http\Request;
+
+class VisitorController extends Controller
+{
+
+    public function index(){
+
+        return view('pages.index');
+    }
+    public function investorList(){
+
+        return view('pages.visitor_list');
+    }
+
+
+    public function create()
+    {
+        //
+        return view('pages.create_visitor');
+    }
+
+
+    public function store(Request $request)
+    {
+        $visitors = new Visitor();
+
+        $visitors->name = $request->name;
+        $visitors->phone = $request->phone;
+        $visitors->purpose = $request->purpose;
+        $visitors->time = $request->time;
+
+        $visitors->save();
+
+        return redirect('create-visitor')->with(['success' => 'New Visitor created successfully']);
+    }
+
+
+    public function show()
+    {
+        $visitors = Visitor::paginate(4);
+        return view('pages.visitor_list',['visitors'=>$visitors]);
+
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container text-center mt-5  ">
+
+    <div class="container">
+        <h2>Integrative Programming and Technology BSIT-3</h2>
+        <h1>Godwin Ramos Atabelo</h1>
+        <div class="container">
+            <h4>Semi Final Project</h4>
+        </div>
+    </div>
+
+
+
+</div>
+@endsection
